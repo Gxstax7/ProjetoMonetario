1f139da86ea558059bc6c5c9c71acb40d4445d51
gxstax7 <gustavodias0904@gmail.com>
2025-10-04 14:51:35 -0300

    finalizados rotas do controller de Devedor

--- monetario/src/main/java/spring/controle/monetario/controller/DevedorController.java
+++ monetario/src/main/java/spring/controle/monetario/controller/DevedorController.java
@@ -1,5 +1,12 @@
 package spring.controle.monetario.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import spring.controle.monetario.model.Devedor;
 import spring.controle.monetario.service.DevedorService;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/devedor")
@@ -21,6 +30,28 @@ public class DevedorController {
     @PostMapping("/criarDevedor")
         public Devedor criarDevedor(@RequestBody Devedor devedor){
             return devedorService.criarDevedor(devedor);
+        }
+    
+    @GetMapping("/listarDevedores/{id}")
+        public Optional<Devedor> buscarDevedor(@PathVariable long id){
+            return devedorService.buscarDevedor(id);
+        }
+    
+
+    @GetMapping("/listarDevedores")
+        public List<Devedor> buscaGeral(){
+            return devedorService.mostrarDevedores();
+        }
+    
+
+    @DeleteMapping("/excluirDevedor/{id}")
+        public ResponseEntity<Void> deletarDevedor(@PathVariable long id){
+            return devedorService.deletarDevedor(id);
+        }
+
+    @GetMapping("/atualizarDevedor/{id}")
+        public ResponseEntity<Void> atualizarDevedor(@RequestBody Devedor devedor, @PathVariable long id) {
+            return devedorService.atualizarDevedor(devedor, id);   
         }
     
 
